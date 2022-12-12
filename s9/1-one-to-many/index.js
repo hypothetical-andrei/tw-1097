d47cf22aeec427195df3326d4b967cc2ef2b56d0
AT <hypothetical.andrei@gmail.com>
2022-12-12 13:29:00 +0200

    .

--- s9/1-one-to-many/index.js
+++ s9/1-one-to-many/index.js
@@ -101,6 +101,44 @@ app.get('/universities/:universityId/students', async (req, res, next) => {
   }
 })
 
+/**
+ * GET all the students from a university using include.
+ */
+ app.get('/universities/:universityId/students/:studentId', async (req, res, next) => {
+  try {
+    const university = await University.findByPk(req.params.universityId, {
+      include: [{
+        model: Student,
+        where: {
+          id: req.params.studentId
+        }
+      }]
+    })
+    const students = university.students
+    const student = students.shift()
+    // const university = await University.findByPk(req.params.universityId)
+    // if (university) {
+    //   const students = await university.getStudents({
+    //     where: {
+    //       id: req.params.studentId
+    //     }
+    //   })
+    //   const student = students.shift()
+    // const student = await Student.findByPk(req.params.studentId, {
+    //   where: {
+    //     universityId: req.params.universityId
+    //   }
+    // })
+    if (student){
+      res.status(200).json(student)
+    } else {
+      res.status(404).json({ message: '404 - University Not Found!' })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 // Create a middleware to handle 500 status errors.
 app.use((err, req, res, next) => {
   console.error('[ERROR]:' + err)
